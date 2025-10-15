985fc74eb8e5bb9d750f6ae27d6842caaa0af769
Дмитрий Чиликин <dmitrijcilikin@MacBook-Air-Dmitrij.local>
2025-10-15 14:06:37 +0200

    Dodano migracje bazy danych (Flask-Migrate)

--- app.py
+++ app.py
@@ -9,6 +9,8 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///products.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
+from flask_migrate import Migrate
+migrate = Migrate(app, db)
 with app.app_context():
     db.create_all()
 
